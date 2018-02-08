6f3c1eb2803b5e0f1825e58962c6062f720a56e7
Alex Masson <amasson99@gmail.com>
2018-02-08 11:50:48 -0600

    practice 1

---
+++ practice.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class practice{
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+
+      String name;
+      String input;
+      Int age = 0;
+
+      System.out.print("Enter your name: ");
+      name = in.nextLine();
+
+      System.out.print("How old are you? ");
+      input = in.nextLine();
+      age = Integer.parseInt(input);
+
+      System.out.println("Hello, " + name + ". You will be " + (age + 1) + "on your next birthday");
+
+    in.close();
+  }
+}
