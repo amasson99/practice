a2448717ece6646e4566b1cf8755a009640d673d
Alex Masson <amasson99@gmail.com>
2018-02-08 12:14:25 -0600

    a

--- practice2.java
+++ practice2.java
@@ -1,11 +1,13 @@
 public class practice2{
   public static void main(String [] args){
     int total = 0;
+    int prev = 0;
     int i = 0;
-    for (i = 0; i <= 1000; i+=2){
-      total = total + i;
+    for (i = 0; i <= 1000; i = i + 2){
+      prev = i;
+      total += prev;
     }
-    System.out.println(i);
+    System.out.println(total);
 
   }
 }
